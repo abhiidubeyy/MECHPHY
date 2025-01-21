ed4e0fbb0d11d402ecf015f88614b9eb43021909
yash110606 <144436949+yash110606@users.noreply.github.com>
2025-01-21 12:38:15 +0530

    Update IR_receiver.c
    the updated faster algorithm to receive the 16-bit data and 16-bit inverted data

--- Vest/IR_receiver.c
+++ Vest/IR_receiver.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
+#define SIZE 33
+
 uint32_t * RCC_AHB2ENR = (uint32_t *)0x4002104c;
 uint32_t * RCC_APB1ENR = (uint32_t *)0x40021058;
 uint32_t * TIM2_CR1 = (uint32_t *)0x40000000;
@@ -14,172 +16,82 @@ uint32_t * TIM2DIER = (uint32_t *)0x4000000c;
 uint32_t * NVICISER = (uint32_t *)0xe000e100;
 volatile uint32_t * TIM2_IFR = (uint32_t *)0x40000010;
 volatile uint32_t * TIM2CCR1 = (uint32_t *)0x40000034;
-volatile uint32_t edge_u = 0x00000000;
-volatile uint32_t edge_d = 0x00000000;
-volatile uint32_t time_gap = 0x00000000;
-volatile int pin_state = 1;
 volatile uint32_t * GA_OPDR = (uint32_t *)0x48000014;
-volatile int header_;
-int check_header();
-int check_bits();
-int check_integrity();
-volatile int a[8] = {0,0,0,0,0,0,0,0};
-volatile int i_a[8] = {1,1,1,1,1,1,1,1};
+volatile uint16_t a[SIZE];
+volatile uint16_t previous_edge = 0x0000;
+volatile uint16_t current_edge = 0x0000;
+volatile uint8_t change_flag = 0;
+volatile uint16_t time;
+volatile uint8_t ind = 0;
+volatile uint8_t header = 0;
 int main()
 {
 	*RCC_AHB2ENR |= 0x01;
 	*RCC_APB1ENR |= 0x01;
 	*GA_MODER &= ~(0x0f<<10);
-	*GA_MODER |= (0x02<<10)|(0x01<<12);
+	*GA_MODER &= ~(0x0f);
+	*GA_MODER |= (0x02<<10)|(0x01);
 	*GA_PUPDR |= (0x01<<10);
 	*GA_AFRL |= (0x01<<20);
 	*TIM2CCMR |= (0x01);
 	*TIM2CCER |= (0x03);
-	*TIM2CCER |= (0x08);
-	*TIM2_ARR = (0x9C3F);
+	*TIM2CCER &= ~(0x08);
+	*TIM2_ARR = (0xEA5F);
 	*TIM2_PSC = (0x00);
 	*TIM2DIER |= (0x02);
 	*TIM2_CR1 |= (0x01<<7)|(0x01);
 	*NVICISER |= (0x01<<28);
 	while(1)
 	{
-		while(pin_state){};
-		while(!pin_state){};
-		int x = check_header();
-		if(x==0)
+		while(!(change_flag));
+		change_flag = 0;
+		if(previous_edge<current_edge)
 		{
-			int y = check_bits();
-			if(y == 1)
+			time = current_edge-previous_edge;
+		}
+		else
+		{
+			time = (0xffff-previous_edge)+current_edge;
+		}
+		if(header==0)
+		{
+			if(time >= 0xCB20 && time <= 0xDAC0)
 			{
-				int  z = check_integrity();
-				if(z == 1)
-				{
-					*GA_OPDR |= (0x01<<6);
-					a[0]=0;
-					a[1]=0;
-					a[2]=0;
-					a[3]=0;
-					a[4]=0;
-					a[5]=0;
-					a[6]=0;
-					a[7]=0;
-				}
+				header = 1;
+				ind = 0;
 			}
 			else
 			{
-				*GA_OPDR &= ~(0x01<<6);
-				continue;
+				header = 0;
+				ind = 0;
 			}
 		}
 		else
 		{
-			*GA_OPDR &= ~(0x01<<6);
-			continue;
+			if (time >= 0xFA0 && time <=0x1770)
+			{
+				a[ind]=0;
+			}
+			else if(time >= 0X20D0 && time <=0x2580)
+			{
+				a[ind]=1;
+			}
+			if(ind==32)
+			{
+				ind = 0;
+				header = 0;
+				*GA_OPDR |= (0x01);
+				continue;
+			}
+			ind++;
 		}
 	}
 }
 
 void TIM2_IRQHandler(void)
 {
-	if(pin_state == 1)
-	{
-		pin_state = 0;
-		edge_d  = *TIM2CCR1;
-		*TIM2CCER &= ~(0x02);
-	}
-	else
-	{
-		pin_state = 1;
-		edge_u = *TIM2CCR1;
-		*TIM2CCER |= (0x02);
-	}
+	change_flag = 1;
+	previous_edge = current_edge;
+	current_edge = *TIM2CCR1;
 	*TIM2_IFR &= ~(0x02);
-}
-
-int check_header()
-{
-	if(edge_u>edge_d)
-	{
-		time_gap = edge_u - edge_d;
-	}
-	else
-	{
-		time_gap = (0x9C3F - edge_d)+edge_u;
-	}
-	if(time_gap >= 0x84D0 && time_gap <= 0x8CA0)
-	{
-		while(pin_state);
-		if(edge_u<edge_d)
-		{
-			time_gap = edge_d - edge_u;
-		}
-		else
-		{
-			time_gap = (0x9C3F - edge_u)+edge_d;
-		}
-		if(time_gap >= 0x3B60 && time_gap <= 0x47E0)
-		{
-			header_ = 1;
-		}
-		else
-		{
-			return -1;
-		}
-	}
-	else
-	{
-		return -1;
-	}
-	return 0;
-}
-
-int check_bits()
-{
-	for(int j = 0;j<8;j++)
-	{
-		while(!pin_state);
-		if(edge_u>edge_d)
-		{
-			time_gap = edge_u - edge_d;
-		}
-		else
-		{
-			time_gap = (0x9C3F - edge_d)+edge_u;
-		}
-		if(time_gap >= 0x708 && time_gap <= 0xA28)
-		{
-			while(pin_state);
-			if(edge_u<edge_d)
-			{
-				time_gap = edge_d - edge_u;
-			}
-			else
-			{
-				time_gap = (0x9C3F - edge_u)+edge_d;
-			}
-			if(time_gap >= 0x708 && time_gap <= 0xA28)
-			{
-				a[j] = 0;
-				i_a[j] = 1;
-			}
-			else if(time_gap >= 0x1388 && time_gap <= 0x1B58)
-			{
-				a[j] = 1;
-				i_a[j] = 0;
-			}
-		}
-	}
-	return 1;
-}
-
-int check_integrity()
-{
-	for(int j = 0;j<8;j++)
-	{
-		if(a[j]==i_a[j])
-		{
-			return -1;
-		}
-	}
-	return 1;
 }
